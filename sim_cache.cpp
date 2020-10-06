af20c8138cba453e8af805ce0078bf5de0c0409d
Raj Parekh <rajparekh001@outlook.com>
2020-10-06 13:36:35 -0400

    fixed error with index bits

--- sim_cache.cpp
+++ sim_cache.cpp
@@ -88,9 +88,9 @@ struct CACHE{
     }
     BIT l1_cache_address = calcBit(L1_cache_set, my_cache.BLOCKSIZE);
     std::cout << "printing address bits" << "\n";
-    std::cout << l1_cache_address.indexBit << "\n";
-    std::cout << l1_cache_address.blockoffsetBit << "\n";
-    std::cout << l1_cache_address.tagBit << "\n";
+    std::cout << "index bits: " << l1_cache_address.indexBit << "\n";
+    std::cout << "blockoffseet bits: " << l1_cache_address.blockoffsetBit << "\n";
+    std::cout << "tag bits: " << l1_cache_address.tagBit << "\n";
     std::cout << "end of address bits" << "\n";
     // End cache parameters constraint check
 
@@ -113,14 +113,16 @@ struct CACHE{
     //split_address l2_decode;
     addressInBits l1_bits;
     //std::string addressInBits;
-   
+   int fileLen;
     if(myfile.is_open()){
         while (getline (myfile, line)){
+            
             if(line.empty()){
                 continue;
             }
             std::cout << line << "\n";
             std::istringstream stm(line);
+            input.clear();
             while(stm >> token){
                 input.push_back(token);
             }
@@ -129,15 +131,20 @@ struct CACHE{
             std::cout << input[1] << "\n";
             hexAd = strtoul (input[1].c_str(), 0, 16);
             l1_bits = addBits(hexAd, l1_cache_address.tagBit,l1_cache_address.indexBit);
-            
-            break;
+            std::cout << std::hex << "address: "<< l1_bits.adrBits << "\n";
+            std::cout << std::hex << "tag: " << l1_bits.tBits << "\n";
+            std::cout << std::dec << "index: " << l1_bits.iBits << "\n";
+            fileLen++;
+            if(fileLen > 12){
+                break;}
                     
         }
+    
         myfile.close();
         //std::cout << hexAd << "\n";
-        std::cout << std::hex << l1_bits.adrBits << "\n";
-        std::cout << std::hex << l1_bits.iBits << "\n";
-        std::cout << std::hex << l1_bits.tBits << "\n";
+        //std::cout << std::hex << l1_bits.adrBits << "\n";
+        //std::cout << std::hex << l1_bits.iBits << "\n";
+        //std::cout << std::hex << l1_bits.tBits << "\n";
     }
     else{
         std::cout << "File doesn't exist." << "\n";
@@ -167,19 +174,33 @@ struct BIT calcBit(unsigned int set,unsigned int blocksize){
     return a;
 }*/
  struct addressInBits addBits(unsigned int address_bits,unsigned int tag_bits,unsigned int index_bits){
-     std::bitset<32> adr(address_bits);
-            addressInBits a;
-            std::string mystring = adr.to_string<char,std::string::traits_type,std::string::allocator_type>();
-            a.adrBits = adr.to_ulong();
-            
-           std::bitset<32> adrTag(mystring,0,tag_bits); 
-            
-            a.tBits = adrTag.to_ulong();
-            std::bitset<32> adrIdx(mystring, tag_bits+1, tag_bits+index_bits);
-       
-            a.iBits = adrIdx.to_ulong();
-            return a;
-            //std::cout << "tag bits: " << add2 << "\n";
+    addressInBits a;
+
+    std::bitset<32> adr(address_bits);
+    std::string mystring = adr.to_string<char,std::string::traits_type,std::string::allocator_type>();
+    a.adrBits = adr.to_ulong();
+    std::cout << "address: " << adr << "\n";
+    //std::cout << std::hex << "address in hex: " << a.adrBits << "\n";
+
+    std::bitset<32> adrTag(mystring,0,tag_bits); 
+    std::cout << "tag: " << adrTag << "\n";
+    a.tBits = adrTag.to_ulong();
+    //std::cout << std::hex << "tag in hex: " << a.tBits << "\n";
+    
+    std::bitset<32> adrIdx(mystring,22,6);
+    std::cout << "idx: " << adrIdx << "\n";
+    a.iBits = adrIdx.to_ulong();
+    //std::cout << std::hex << "idx in hex: " << a.iBits << "\n";
+    return a;
+    //std::cout << "tag bits: " << add2 << "\n";
 
  }
+/*
+0100000000000001001101 1000001100
+0100000000000001001101 1000001100
+00000000000100000000000001001101
+00000000000100000000000001001101
+00000000001000000000000010011011
 
+10111 0100
+*/
